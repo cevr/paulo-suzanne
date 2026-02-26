32082fbe6d84746e6ca44c555dbaac10bc745eaa
cevr <seeve.c@gmail.com>
2026-02-26 14:14:41 -0500

    refactor: replace react-router-serve with bun server
    Drop @react-router/serve in favor of a simple Bun.serve() server.
    Remove stale future flags from react-router config. Add @types/bun.

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,15 +1,5 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  // prerender: true,
-  future: {
-    v8_middleware: true,
-    unstable_optimizeDeps: true,
-    unstable_splitRouteModules: true,
-    unstable_subResourceIntegrity: true,
-    unstable_viteEnvironmentApi: true,
-  },
 } satisfies Config;
